aaa766b9aabaddb2731134ceaab89b29f1c90d9f
Darwin Yang <zoomdarwin@gmail.com>
2023-12-03 02:02:21 -0500

    Update template.

--- helpers/template.py
+++ helpers/template.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 def input_to_list():
   with open(Path(__file__).with_name('input.txt')) as f:
-    lines = [line for line in f]
-  return lines
+    return [line.strip() for line in f]
 
 def main():
   input = input_to_list()
 
-main()
+if __name__ == "__main__":
+  main()
